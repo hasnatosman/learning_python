4455c6389ae7d1a4b4e1ae91f927b8be327c8b48
HASNAT <61647150+hasnatosman@users.noreply.github.com>
2022-05-27 23:34:55 +0600

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,10 @@
+print(" Hello World")
+print(" Welcome to pyhton again !")
+
+
+
+name = input("What is your name ? ")
+print("My name is: ", name)
+
+print("Hey, ", name, " !!!")
+print("Nice to meet you again. !!")
