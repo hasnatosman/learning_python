9cacb60b100bd568f7d5659e09ac20288438529e
HASNAT <61647150+hasnatosman@users.noreply.github.com>
2022-06-07 22:47:45 +0600

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,11 @@
+# calling variables............................
+
+variable1 = 5
+variable2 = 6
+
+# adding variable1 & variable2.................
+
+result = variable1 + variable2
+
+#printing result...............................
+print("Sum of two variable is: ",result)
